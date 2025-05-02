5da6daffe71486c09a16d56c01120a34eacd8d62
Julian L <jthemage101@gmail.com>
2025-05-02 13:29:32 -0400

    chore: add more game roles

--- src/defaults.ts
+++ src/defaults.ts
@@ -3,5 +3,10 @@ export const GAME_ROLES: Record<string, string> = {
     minecraft: '1112385912039559208',
     cs2: '825552434591301633',
     valorant: '825552991708250112',
-    valheim: ''
+    valheim: '1367915500222414858',
+    deadlock: '1283568191691559013',
+    overwatch: '1067618049324236810',
+    apex: '920687345060171907',
+    lethalcompany: '1184360149511577610',
+    bloons: '1184360149511577610',
 }
